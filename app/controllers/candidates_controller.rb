ead8beddf3c349b188e2ddf69aa3fb365cdaadd6
BillKung <billkung.happy@gmail.com>
2017-04-29 16:26:55 +0800

    Add Method new and create successfully

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -5,4 +5,21 @@ class CandidatesController < ApplicationController
 	def new
 		@candidate=Candidate.new
 	end
+	def create
+		@candidate=Candidate.new(get_candidate_params)
+		@candidate.save
+		if @candidate.save
+			redirect_to candidates_path, notice: "新增候選人成功"
+		else
+			render :new
+		end
+		
+	end
+	
+
+	private
+	def get_candidate_params
+		params.require(:candidate).permit(:name,:age,:party,:politics)
+		# .permit 是過濾器，只准許括號內的物件通過
+	end
 end
