a7a92fb41ad9db156cceb966eb501cf3b005079d
BillKung <billkung.happy@gmail.com>
2017-04-29 17:37:35 +0800

    Add all method to the App

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,5 @@
 class CandidatesController < ApplicationController
+	before_action :find_candidate_byid, :only=>[:edit,:update,:destroy,:vote]
 	def index
 		@candidates=Candidate.all
 	end
@@ -15,11 +16,41 @@ class CandidatesController < ApplicationController
 		end
 		
 	end
+
+	def edit
+	# before_action
+	end
+	def update
+	# before_action
+		if @candidate.update(get_candidate_params)
+			redirect_to candidates_path, :notice =>"編輯候選人成功"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+	# before_action
+		@candidate.delete if @candidate
+		redirect_to candidates_path, :notice =>"刪除候選人成功"
+	end
+
+	def vote
+	# before_action
+		@candidate.increment(:votes)
+		# @candidate.votes+=1
+		@candidate.save
+		redirect_to candidates_path, notice: "完成投票!"
+	end
+
 	
 
 	private
 	def get_candidate_params
 		params.require(:candidate).permit(:name,:age,:party,:politics)
 		# .permit 是過濾器，只准許括號內的物件通過
+	end
+	def find_candidate_byid
+		return Candidate.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'candidates#index'
-  resources :candidates
+  # post '/candidates/vote' => 'candidates/vote'
+  resources :candidates do
+  	member do
+      post :vote
+  	end
+  end
 
 end
